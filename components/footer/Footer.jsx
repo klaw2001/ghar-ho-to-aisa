7fd44d18d7891aa2cc98c9ad8f6ca0b0008091c4
klaw2001 <netke2611@gmail.com>
2024-06-29 04:42:25 +0530

    new footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -11,48 +11,48 @@ const Footer = () => {
       href: "/",
       text: "Home",
       className:
-        "manrope font-semibold text-lg px-4 py-2 block lg:inline-block text-white",
+        "manrope font-semibold text-lg px-4 py-2 block lg:inline-block text-black",
     },
     {
       href: "/about",
       text: "About Us",
       className:
-        "manrope font-semibold text-lg px-4 py-2 block lg:inline-block text-white",
+        "manrope font-semibold text-lg px-4 py-2 block lg:inline-block text-black",
     },
     {
       href: "/investors",
       text: "Investors",
       className:
-        "manrope font-semibold text-lg px-4 py-2 block lg:inline-block text-white",
+        "manrope font-semibold text-lg px-4 py-2 block lg:inline-block text-black",
     },
     {
       href: "/innovations",
       text: "Innovations",
       className:
-        "manrope font-semibold text-lg px-4 py-2 block lg:inline-block text-white",
+        "manrope font-semibold text-lg px-4 py-2 block lg:inline-block text-black",
     },
     {
       href: "/projects",
       text: "Projects",
       className:
-        "manrope font-semibold text-lg px-4 py-2 block lg:inline-block text-white",
+        "manrope font-semibold text-lg px-4 py-2 block lg:inline-block text-black",
     },
     {
       href: "/blogs",
       text: "Blogs",
       className:
-        "manrope font-semibold text-lg px-4 py-2 block lg:inline-block text-white",
+        "manrope font-semibold text-lg px-4 py-2 block lg:inline-block text-black",
     },
   ];
 
   return (
-    <footer className="py-16 bg-purple">
-      <div className="container">
-        <div className="footer-wrap">
-          <div className="footer-one flex flex-col md:flex-row justify-between items-center">
-            <div className="w-full md:w-1/2 text-center md:text-left">
+    <footer className="bg-purple">
+      <div className="footer-wrap">
+        <div className="py-16 bg-[url('/images/footer-bg.png')] bg-cover	">
+          <div className="container mx-auto footer-one flex flex-col md:flex-row justify-between items-center">
+            <div className="w-full md:w-3/5 text-center md:text-left">
               <h1 className="manrope text-white">Join Our Parivaar</h1>
-              <p className="manrope text-white">
+              <p className="manrope text-white md:mr-40">
                 By putting the customer at the heart of everything we do, we not
                 only deliver exceptional results but also forge meaningful and
                 lasting relationships. This customer-first approach is the
@@ -60,97 +60,102 @@ const Footer = () => {
                 towards excellence.
               </p>
             </div>
-            <div className="w-full md:w-1/2 text-center md:text-right my-4 md:my-0">
-              <h1 className="manrope text-white">Subscribe</h1>
-              <div className="newsletter-box flex gap-2 md:justify-end justify-center mb-3">
-                <input
-                  type="text"
-                  placeholder="Enter Your Email"
-                  className="p-3 manrope"
-                />
-                <button
-                  type="submit"
-                  className="app-btn bg-[#C69E58] manrope para-small"
-                >
-                  Submit
-                </button>
-              </div>
+            <div className="w-full md:w-2/5 text-center md:text-right my-4 md:my-0">
+              <div className="md:w-max md:text-left md:ml-auto">
+                <h1 className="manrope text-white">Subscribe</h1>
+                <div className="newsletter-box flex gap-2 md:justify-start justify-center mb-3">
+                  <input
+                    type="text"
+                    placeholder="Enter Your Email"
+                    className="p-3 manrope md:w-[353px]"
+                  />
+                  <button
+                    type="submit"
+                    className="app-btn bg-[#C69E58] manrope para-small"
+                  >
+                    Submit
+                  </button>
+                </div>
               <p className="small-para manrope text-white">
                 By subscribing you agree to with our Privacy Policy
               </p>
-            </div>
-          </div>
-          <div className="footer-two flex flex-col md:flex-row justify-between items-start md:py-8 md:px-12 p-8 my-8 border-2 border-inherit">
-            <div>
-              <Image
-                src="/images/footer-logo.png"
-                width={200}
-                height={80}
-                className="object-contain mix-blend-lighten"
-              />
-              <div className="content-box text-white manrope my-3">
-                <p className="manrope font-bold">Address:</p>
-                <span className="text-sm manrope">
-                  6th Flr, Classic Pentagon, W.E. Highway, Next to Bisleri
-                  Factory, Andheri (East), Mumbai - 400 099.
-                </span>
-              </div>
-              <div className="content-box text-white manrope">
-                <p className="manrope font-bold">Contact:</p>
-                <a className="text-sm manrope" href="tel:+91 22 42275700">
-                  +91 22 42275700
-                </a>{" "}
-                <br />
-                <a
-                  className="text-sm manrope"
-                  href="mailto:info@gharhotoaisa.com"
-                >
-                  info@gharhotoaisa.com
-                </a>
-              </div>
-              <div className="icons-box flex items-center gap-3 mt-3">
-                <span>
-                  <FaFacebook className="text-2xl text-white" />
-                </span>
-                <span>
-                  <FaInstagram className="text-2xl text-white" />
-                </span>
-                <span>
-                  <FaYoutube className="text-2xl text-white" />
-                </span>
-              </div>
-            </div>
-            <div className="mt-5 md:mt-0">
-              <div className="flex gap-4 items-center justify-between md:justify-end">
-                <ul>
-                  {links.map((elem, ind) => (
-                    <li key={ind}>
-                      <Link href={elem.href} className={elem.className}>
-                        {elem.text}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-                <ul>
-                  {links.map((elem, ind) => (
-                    <li key={ind}>
-                      <Link href={elem.href} className={elem.className}>
-                        {elem.text}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
               </div>
             </div>
           </div>
-          <div className="credits flex flex-col md:flex-row justify-between items-center">
-            <div>
-              <p className="para-small text-white">©Ghar Ho To Aisa 2024 All rights reserved.</p>
+        </div>
+        <div className="footer-two bg-white flex flex-col md:flex-row justify-between items-start md:py-14 md:px-12 p-8  rounded-ss-[3rem]	rounded-se-[3rem]	">
+          <div>
+            <Image
+              src="/images/footer-logo.png"
+              width={200}
+              height={80}
+              className="object-contain mb-8 mx-auto md:mr-auto md:ml-0"
+            />
+            <div className="content-box manrope my-6">
+              <p className="manrope font-bold text-purple">Address:</p>
+              <span className="text-sm manrope">
+                6th Flr, Classic Pentagon, W.E. Highway, Next to Bisleri
+                Factory, Andheri (East), Mumbai - 400 099.
+              </span>
             </div>
-            {/* <div className="flex gap-3">
+            <div className="content-box manrope">
+              <p className="manrope font-bold text-purple">Contact:</p>
+              <a className="text-sm manrope" href="tel:+91 22 42275700">
+                +91 22 42275700
+              </a>{" "}
+              <br />
+              <a
+                className="text-sm manrope"
+                href="mailto:info@gharhotoaisa.com"
+              >
+                info@gharhotoaisa.com
+              </a>
+            </div>
+            <div className="icons-box flex items-center gap-3 mt-3">
+              <span>
+                <FaFacebook className="text-2xl text-white" />
+              </span>
+              <span>
+                <FaInstagram className="text-2xl text-white" />
+              </span>
+              <span>
+                <FaYoutube className="text-2xl text-white" />
+              </span>
+            </div>
+          </div>
+          <div className="mt-5 md:mt-0">
+            <div className="flex gap-4 items-center justify-between md:justify-end">
+              <ul>
+                {links.map((elem, ind) => (
+                  <li key={ind}>
+                    <Link href={elem.href} className={elem.className}>
+                      {elem.text}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+              <ul>
+                {links.map((elem, ind) => (
+                  <li key={ind}>
+                    <Link href={elem.href} className={elem.className}>
+                      {elem.text}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="credits flex flex-col md:flex-row justify-between items-center bg-white ">
+          <div className="border-t-2 border-black	w-full mx-6 flex py-4">
+            <p className="para-small text-center w-full text-black">
+              ©Ghar Ho To Aisa 2024 All rights reserved.
+            </p>
+           
+          </div>
+          {/* <div className="flex gap-3">
               Something
             </div> */}
-          </div>
         </div>
       </div>
     </footer>
